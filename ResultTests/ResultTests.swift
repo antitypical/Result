2fd2250a2af20b29a00509f2ed54fc3397f9ef00
Rob Rix <rob.rix@github.com>
2015-02-22 23:54:03 -0500

    Test try for failure cases.

--- ResultTests/ResultTests.swift
+++ ResultTests/ResultTests.swift
@@ -8,6 +8,30 @@ final class ResultTests: XCTestCase {
 	func testMapRewrapsFailures() {
 		XCTAssertEqual(failure.map(count) ?? 0, 0)
 	}
+
+
+	// MARK: Cocoa API idioms
+
+	func testTryProducesFailuresForBooleanAPIWithErrorReturnedByReference() {
+		let result = try { attempt(true, succeed: false, error: $0) }
+		XCTAssertFalse(result ?? false)
+		XCTAssertNotNil(result.failure)
+	}
+
+	func testTryProducesFailuresForObjectAPIWithErrorReturnedByReference() {
+		let result = try { attempt(1, succeed: false, error: $0) }
+		XCTAssertEqual(result ?? 0, 0)
+		XCTAssertNotNil(result.failure)
+	}
+}
+
+func attempt<T>(value: T, #succeed: Bool, #error: NSErrorPointer) -> T? {
+	if succeed {
+		return value
+	} else {
+		error.memory = Result<()>.error()
+		return nil
+	}
 }
 
 let success = Result.success("success")
