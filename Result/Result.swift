ed7da6f4dc1750085c9b9db2ba58ca5810a8543f
Rob Rix <rob.rix@github.com>
2015-02-22 19:38:32 -0500

    Static convenience constructors.

--- Result/Result.swift
+++ Result/Result.swift
@@ -2,18 +2,27 @@
 
 /// An enum representing either a failure with an explanatory error, or a success with a result value.
 public enum Result<T>: EitherType {
+	public static func failure(error: NSError) -> Result {
+		return Failure(error)
+	}
+
+	public static func success(value: T) -> Result {
+		return Success(Box(value))
+	}
+
+
 	case Failure(NSError)
 	case Success(Box<T>)
 
 
 	// MARK: EitherType
 
-	public static func left(value: NSError) -> Result {
-		return Failure(value)
+	public static func left(error: NSError) -> Result {
+		return failure(error)
 	}
 
 	public static func right(value: T) -> Result {
-		return Success(Box(value))
+		return success(value)
 	}
 
 	public func either<Result>(ifLeft: NSError -> Result, _ ifRight: T -> Result) -> Result {
