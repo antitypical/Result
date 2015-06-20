965c93a8b67058bc8a3f9dd54226dd1d4716caf7
Thomas Visser <thomas.visser@gmail.com>
2015-06-20 16:19:54 +0200

    Added documentation

--- Result/ResultType.swift
+++ Result/ResultType.swift
@@ -1,23 +1,30 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
+/// A type that can represent either failure with an error or success with a result value.
 public protocol ResultType {
 	typealias Value
 	typealias Error: ErrorType
 	
+	/// Constructs a succesful result wrapping a `value`.
 	init(value: Value)
+
+	/// Constructs a failed result wrapping an `error`.
 	init(error: Error)
 	
+	/// Case analysis for ResultType.
+	///
+	/// Returns the value produced by appliying `ifFailure` to the error if self represents a failure, or `ifSuccess` to the result value if self represents a success.
 	func analysis<U>(@noescape ifSuccess ifSuccess: Value -> U, @noescape ifFailure: Error -> U) -> U
 }
 
 public extension ResultType {
 	
-	/// Returns the value from `Success` Results, `nil` otherwise.
+	/// Returns the value if self represents a success, `nil` otherwise.
 	var value: Value? {
 		return analysis(ifSuccess: { $0 }, ifFailure: { _ in nil })
 	}
 	
-	/// Returns the error from `Failure` Results, `nil` otherwise.
+	/// Returns the error if self represents a failure, `nil` otherwise.
 	var error: Error? {
 		return analysis(ifSuccess: { _ in nil }, ifFailure: { $0 })
 	}
