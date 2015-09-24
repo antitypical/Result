cf5e2057244a5ad8a226fd49dbefb0d000adbe63
Rob Rix <rob.rix@github.com>
2015-09-24 12:07:09 -0400

    Declare value/error in the protocol.
    This allows conforming types to override them in generic contexts. This
    comes at the cost of an indirection for each call to them (as the
    compiler can’t inline them) but is more flexible for conforming types.

--- Result/ResultType.swift
+++ Result/ResultType.swift
@@ -15,6 +15,16 @@ public protocol ResultType {
 	///
 	/// Returns the value produced by appliying `ifFailure` to the error if self represents a failure, or `ifSuccess` to the result value if self represents a success.
 	func analysis<U>(@noescape ifSuccess ifSuccess: Value -> U, @noescape ifFailure: Error -> U) -> U
+
+	/// Returns the value if self represents a success, `nil` otherwise.
+	///
+	/// A default implementation is provided by a protocol extension. Conforming types may specialize it.
+	var value: Value? { get }
+
+	/// Returns the error if self represents a failure, `nil` otherwise.
+	///
+	/// A default implementation is provided by a protocol extension. Conforming types may specialize it.
+	var error: Error? { get }
 }
 
 public extension ResultType {
