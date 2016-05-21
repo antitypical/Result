0b1a6c4760cfdb6c78a4c6ef97cc7755bbdf1a3f
JP Simard <jp@jpsim.com>
2016-05-21 13:18:30 -0700

    add another cast to make Dictionary.subscript unambiguous on Linux

--- Tests/Result/ResultTests.swift
+++ Tests/Result/ResultTests.swift
@@ -251,15 +251,15 @@ func tryIsSuccess(text: String?) throws -> String {
 
 extension NSError {
 	var function: String? {
-		return userInfo[NSString(string: Result<(), NSError>.functionKey)] as? String
+		return userInfo[NSString(string: Result<(), NSError>.functionKey) as NSObject] as? String
 	}
 	
 	var file: String? {
-		return userInfo[NSString(string: Result<(), NSError>.fileKey)] as? String
+		return userInfo[NSString(string: Result<(), NSError>.fileKey) as NSObject] as? String
 	}
 
 	var line: Int? {
-		return userInfo[NSString(string: Result<(), NSError>.lineKey)] as? Int
+		return userInfo[NSString(string: Result<(), NSError>.lineKey) as NSObject] as? Int
 	}
 }
 
