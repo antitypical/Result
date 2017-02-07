7013863969ca3cf96fbc5190ac1054bfc3b4c1c4
Norio Nomura <norio.nomura@gmail.com>
2017-02-07 10:09:32 +0900

    Change to use `#if swift(>=3.1)`

--- Result/Result.swift
+++ Result/Result.swift
@@ -120,6 +120,8 @@ public func materialize<T>(_ f: @autoclosure () throws -> T) -> Result<T, AnyErr
 	}
 }
 
+#if swift(>=3.1)
+
 @available(*, unavailable, message: "Use the overload which returns `Result<T, AnyError>` instead")
 public func materialize<T>(_ f: () throws -> T) -> Result<T, NSError> {
 	fatalError("unreachable")
@@ -129,6 +131,24 @@ public func materialize<T>(_ f: () throws -> T) -> Result<T, NSError> {
 public func materialize<T>(_ f: @autoclosure () throws -> T) -> Result<T, NSError> {
 	fatalError("unreachable")
 }
+
+#else
+
+@available(*, deprecated, message: "Use the overload which returns `Result<T, AnyError>` instead")
+public func materialize<T>(_ f: () throws -> T) -> Result<T, NSError> {
+	return materialize(try f())
+}
+
+@available(*, deprecated, message: "Use the overload which returns `Result<T, AnyError>` instead")
+public func materialize<T>(_ f: @autoclosure () throws -> T) -> Result<T, NSError> {
+	do {
+		return .success(try f())
+	} catch let error as NSError {
+		return .failure(error)
+	}
+}
+
+#endif
 
 // MARK: - Cocoa API conveniences
 
