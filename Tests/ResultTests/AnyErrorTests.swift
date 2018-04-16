4dea6203dd5bd28833c96b68c75c0b3875940d0b
Sho Ikeda <suicaicoca@gmail.com>
2018-04-16 13:52:02 +0900

    [Swift 4.1] Generate LinuxMain by running `swift test --generate-linuxmain` on macOS

--- Tests/ResultTests/AnyErrorTests.swift
+++ Tests/ResultTests/AnyErrorTests.swift
@@ -3,10 +3,6 @@ import XCTest
 import Result
 
 final class AnyErrorTests: XCTestCase {
-	static var allTests: [(String, (AnyErrorTests) -> () throws -> Void)] {
-		return [ ("testAnyError", testAnyError) ]
-	}
-
 	func testAnyError() {
 		let error = Error.a
 		let anyErrorFromError = AnyError(error)
